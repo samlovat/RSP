b6a21148e3a144d998a093cc4447186f909f9370
samlovat <a1813474@adelaide.edu.au>
2025-12-10 15:21:17 +1030

    Done LinkedList

--- LinkedList.h
+++ LinkedList.h
@@ -1,38 +1,77 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
-// Linked List Class
+
 #include "Node.h"
 #include <iostream>
-
+// Linked List Class
 class LinkedList {
     public:
         Node* Head;
         LinkedList(Node* Head_){
             this->Head = Head_;
         }
+        LinkedList(int headValue){
+            this->Head = new Node(headValue, nullptr);
+        };
         LinkedList(){
             this->Head = nullptr;
         };
-        void del(int deletee){};
+        Node* searchForPredecessor(int searchee, Node* curr){
+            //Base Case
+            if(curr->nextNode == nullptr){
+                return nullptr;
+            }
+            if(curr->nextNode->data == searchee){
+                return curr;
+            }
+            return this->searchForPredecessor(searchee, curr->nextNode);
+        };
+        void del(int deletee){
+            if(this->Head != nullptr){
+                if(this->Head->data == deletee){
+                    Node* temp = this->Head;
+                    if(this->Head->nextNode != nullptr){
+                        this->Head = temp->nextNode;
+                    }
+                    delete temp;
+                    return;
+                }
+            }else{
+                return;
+            }
+            Node* pred = searchForPredecessor(deletee, this->Head);
+            
+            if(pred == nullptr){
+                std::cerr << "Node doesn't exist" << std::endl;
+                return;
+            }
+            std::cout << "pred: " << pred->data << std::endl;
+            Node* temp = pred->nextNode;
+            if(temp->nextNode != nullptr){
+                pred->nextNode = temp->nextNode;
+            }
+            delete temp;
+            return;
+        };
         void ins(int insertee){
             Node* newNode = new Node(insertee, this->Head);
             this->Head = newNode;
         };
         Node* search(int searchee, Node* curr){
             //Base Case
-            if(curr->nextNode == nullptr){
-                return nullptr;
-            }
             if(curr->data == searchee){
                 return curr;
+            }
+            if(curr->nextNode == nullptr){
+                return nullptr;
             }
             return this->search(searchee, curr->nextNode);
         };
         void print(Node* curr){
+            std::cout << curr->data << " | ";
             if(curr->nextNode == nullptr){
                 return;
             }
-            std::cout << curr->data << " | " << std::endl;
             print(curr->nextNode);
             return;
         }
--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 #include "LinkedList.h"
 
 int main(){
-    Node* theHead = new Node(3, nullptr);
-    LinkedList* myList = new LinkedList(theHead);
+    LinkedList* myList = new LinkedList(3);
     myList->ins(2);
     myList->ins(4);
     myList->ins(5);
     myList->ins(6);
     myList->ins(7);
     myList->print(myList->Head);
+    myList->del(4);
+    printf("\n");
+    myList->print(myList->Head);
+    Node* looking = myList->search(7, myList->Head);
+    if(looking != nullptr){
+        printf("\n");
+        std::cout << looking->data << std::endl;
+    }
 }
